80db6ef4a228679bf313acf546af113303650633
Xomak <mail@xomak.net>
2016-09-14 11:55:38 +0300

    Fixed returning user's id as a string

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -21,11 +21,11 @@ class DataReader:
 
     def get_user_by_index(self, index):
         user_dict = self._data[index]
-        u = User(user_dict[DataFields.user_id], user_dict[DataFields.lists], user_dict[DataFields.people])
+        u = User(int(user_dict[DataFields.user_id]), user_dict[DataFields.lists], user_dict[DataFields.people])
         return u
 
     def get_user_id_by_index(self, index):
-        return self._data[index][DataFields.user_id]
+        return int(self._data[index][DataFields.user_id])
 
     def get_matrix_by_list(self, list_id):
         result = [user[DataFields.lists][list_id] for user in self._data]
